8747741c275f6964e47b8800fc573edb5b229615
unknown <nikolius@inbox.ru>
2022-10-24 17:07:33 +0300

    Запросы/логика по таблицам user_channel и  channel

--- webapp/model.py
+++ webapp/model.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    mail = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
